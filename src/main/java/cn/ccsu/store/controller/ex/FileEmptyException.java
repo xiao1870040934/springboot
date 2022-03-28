c8ebba97b16378cc3744e345fc1267d60dd298c4
xiaoge520 <1870040934@qq.com>
2022-03-28 15:27:57 +0800

    第三次提交

---
+++ src/main/java/cn/ccsu/store/controller/ex/FileEmptyException.java
@@ -0,0 +1,6 @@
+package cn.ccsu.store.controller.ex;/**
+ * @Author 潇洒哥queen
+ * @Date 2022/3/28 15:17
+ * @Version 1.0
+ */public class FileEmptyException {
+}
---
+++ src/main/java/cn/ccsu/store/controller/ex/FileUploadIOException.java
@@ -0,0 +1,6 @@
+package cn.ccsu.store.controller.ex;/**
+ * @Author 潇洒哥queen
+ * @Date 2022/3/28 15:22
+ * @Version 1.0
+ */public class FileUploadIOException {
+}
